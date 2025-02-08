49db3008b0a14f0037a8b15dfffd327c14209128
Baurzhan <ulyozen@gmail.com>
2025-02-09 00:28:49 +0500

    feat: added `ExpiresIn` field to `TokenResponse`

--- src/Quiz.Application/Common/TokenResponse.cs
+++ src/Quiz.Application/Common/TokenResponse.cs
@@ -2,6 +2,8 @@ namespace Quiz.Application.Common;
 
 public class TokenResponse
 {
+    public int? ExpiresIn { get; set; }
+    
     public string? AccessToken { get; set; }
     
     public string? RefreshToken { get; set; }
