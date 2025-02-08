5d63686240d70a589f48d978fe37492a0cffd0ca
Baurzhan <ulyozen@gmail.com>
2025-02-09 00:31:46 +0500

    feat: added `UserInfo` class for user data representation

---
+++ src/Quiz.Application/Common/UserInfo.cs
@@ -0,0 +1,10 @@
+namespace Quiz.Application.Common;
+
+public class UserInfo
+{
+    public string? Id { get; set; }
+    
+    public string? Email { get; set; }
+
+    public string? Role { get; set; }
+}
