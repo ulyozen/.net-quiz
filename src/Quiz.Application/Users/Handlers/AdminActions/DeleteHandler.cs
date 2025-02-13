a612b032f882c013855f975fd6cb5ef93d099258
Baurzhan <ulyozen@gmail.com>
2025-02-13 13:11:34 +0500

    feat: added `DeleteHandler` for user deletion

---
+++ src/Quiz.Application/Users/Handlers/AdminActions/DeleteHandler.cs
@@ -0,0 +1,14 @@
+using MediatR;
+using Quiz.Application.Users.Commands.AdminActions;
+using Quiz.Core.Abstractions;
+using Quiz.Core.Common;
+
+namespace Quiz.Application.Users.Handlers.AdminActions;
+
+public class DeleteHandler(IAdminRepository repo) : IRequestHandler<DeleteCommand, OperationResult>
+{
+    public async Task<OperationResult> Handle(DeleteCommand command, CancellationToken cancellationToken)
+    {
+        return await repo.DeleteUserAsync(command.UserId);
+    }
+}
