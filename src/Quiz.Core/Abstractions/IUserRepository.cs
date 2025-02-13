fc7ed661a39ee8818e6c89cc1c204cd5feb87fc6
Baurzhan <ulyozen@gmail.com>
2025-02-13 13:16:31 +0500

    refactor: moved `IUserRepository` methods to specialized repositories

--- src/Quiz.Core/Abstractions/IUserRepository.cs
+++ src/Quiz.Core/Abstractions/IUserRepository.cs
@@ -1,31 +1,3 @@
-using Quiz.Core.Common;
-using Quiz.Core.Entities;
-
 namespace Quiz.Core.Abstractions;
 
-public interface IUserRepository
-{
-    Task<OperationResult<User>> GetUserByIdAsync(string userId);
-    
-    Task<OperationResult<User>> GetUserByEmailAsync(string email);
-    
-    Task<IEnumerable<User>> GetUsersAsync();
-    
-    Task<OperationResult<User>> GetUserByRefreshTokenAsync(string refreshToken);
-    
-    Task<OperationResult<User>> UserCredentialsAsync(string email, string password, bool rememberMe);
-    
-    Task<OperationResult<User>> AddUserAsync(User user);
-    
-    Task<OperationResult> AddRefreshTokenAsync(User user, string refreshToken, string expiryDate);
-    
-    Task<OperationResult> UpdateRefreshTokenAsync(User user, string oldRefreshToken, string newRefreshToken, string expiryDate);
-    
-    Task<OperationResult> UpdateUserAsync(User user);
-    
-    Task<OperationResult> UpdateUserPasswordAsync(string email, string password);
-    
-    Task<OperationResult> DeleteUserAsync(string userId);
-    
-    Task<OperationResult> DeleteRefreshTokenAsync(string refreshToken);
-}
+public interface IUserRepository { }
