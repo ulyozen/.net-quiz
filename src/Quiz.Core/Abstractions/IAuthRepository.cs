1aa4a963e39115ed7dd93b8cf2a42d47954fecc0
Baurzhan <ulyozen@gmail.com>
2025-02-13 12:55:35 +0500

    refactor: updated `IAuthRepository` interface, added user and token management methods

--- src/Quiz.Core/Abstractions/IAuthRepository.cs
+++ src/Quiz.Core/Abstractions/IAuthRepository.cs
@@ -5,11 +5,17 @@ namespace Quiz.Core.Abstractions;
 
 public interface IAuthRepository
 {
-    Task<OperationResult<User>> Create(User user);
+    Task<OperationResult<User>> GetUserAsync(string refreshToken);
     
-    Task<OperationResult<User>> Login(string email, string password, bool rememberMe);
+    Task<OperationResult<User>> AddUserAsync(User user);
     
-    Task<OperationResult> ForgotPassword(string email, string password);
+    Task<OperationResult<User>> LoginAsync(string email, string password, bool rememberMe);
     
-    Task<OperationResult> Logout();
+    Task<OperationResult> AddRefreshTokenAsync(User user, string refreshToken, string expiryDate);
+    
+    Task<OperationResult> UpdateRefreshTokenAsync(User user, string oldRefreshToken, string newRefreshToken, string expiryDate);
+    
+    Task<OperationResult> RecoverPasswordAsync(string email, string password);
+    
+    Task<OperationResult> RevokeAccessAsync();
 }
