45e4986b0fb6c0b18613fc35989d8e4917c31fb1
Baurzhan <ulyozen@gmail.com>
2025-02-13 13:28:51 +0500

    feat: added `AdminController` for user management

--- src/Quiz.Api/Controllers/AdminController.cs
+++ src/Quiz.Api/Controllers/AdminController.cs
@@ -1,11 +1,53 @@
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Quiz.Application.Users.Commands.AdminActions;
+using Quiz.Application.Users.Queries;
 
 namespace Quiz.Api.Controllers;
 
-[ApiController]
 [Route("admin")]
+[ApiController]
+[Authorize(Roles = "admin")]
 [Produces("application/json")]
-public class AdminController : ControllerBase
+public class AdminController(IMediator mediator) : ControllerBase
 {
+    [HttpGet("users")]
+    public async Task<IActionResult> GetUsers([FromQuery] GetUsersQuery query)
+    {
+        var result = await mediator.Send(query);
+        return Ok(result);
+    }
     
+    [HttpPost("user/{id}/ban")]
+    public async Task<IActionResult> BlockUser(string id)
+    {
+        var command = new BlockCommand { UserId = id };
+        var result = await mediator.Send(command);
+        return Ok(result);
+    }
+    
+    [HttpPost("user/{id}/unban")]
+    public async Task<IActionResult> UnblockUser(string id)
+    {
+        var command = new UnblockCommand { UserId = id };
+        var result = await mediator.Send(command);
+        return Ok(result);
+    }
+    
+    [HttpPost("user/{id}/change-role")]
+    public async Task<IActionResult> ChangeRole(string id, ChangeRoleCommand request)
+    {
+        var command = new ChangeRoleCommand { UserId = id, Role = request.Role };
+        var result = await mediator.Send(command);
+        return Ok(result);
+    }
+    
+    [HttpDelete("user/{id:guid}")]
+    public async Task<IActionResult> DeleteUser(string id)
+    {
+        var command = new DeleteCommand { UserId = id };
+        await mediator.Send(command);
+        return Ok();
+    }
 }
