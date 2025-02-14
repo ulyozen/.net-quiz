32c6f68a14e06ed43174fee36d9b612acf444030
Baurzhan <ulyozen@gmail.com>
2025-02-14 12:21:18 +0500

    feat: added Redis configuration and environment variables

---
+++ src/Quiz.Persistence/Common/RedisOptions.cs
@@ -0,0 +1,10 @@
+namespace Quiz.Persistence.Common;
+
+public class RedisOptions
+{
+    public string? Host { get; set; }
+    
+    public string? Port { get; set; }
+    
+    public string? Password { get; set; }
+}
